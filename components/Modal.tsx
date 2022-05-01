4f187eda95b03f929ab8338d3c615f613952f37e
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-05-01 22:14:57 +0100

    added image added drag and drop modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+import Icon from "../modalIcon.svg";
+import Draggable from "react-draggable";
 
 interface Props {
   name: String;
+  display: Boolean;
+  updateDisplay: Function;
 }
 
 const Modal = (props: Props) => {
   return (
-    <div className="w-4/6 bg-slate-400 border-2 border-pink-300">
-      <div className="bg-pink-300 h-6">{props.name}</div>
-      <p>Fack off</p>
-    </div>
+    <Draggable handle=".headerBar">
+      <div
+        className={`absolute z-50 w-4/6 bg-slate-400 border-2 border-pink-300 mx-auto ${
+          props.display ? `` : `hidden`
+        }`}
+      >
+        <div className="headerBar p-1">
+          <Image src={Icon} height={"20"} width={"20"} />
+          {props.name}{" "}
+          <button
+            className="h-6 w-6 mb-1 bg-gray-300 float-right border-2 border-t-gray-100 border-r-gray-100 border-l-gray-600 border-b-gray-600"
+            onClick={() => props.updateDisplay(false)}
+          >
+            X
+          </button>
+        </div>
+        <div className="border-t-2 border-pink-300">
+          <p>Fack off</p>
+        </div>
+      </div>
+    </Draggable>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import Link from "next/link";
 import Head from "next/head";
 import Modal from "../components/Modal";
+import { useState } from "react";
 
 const IndexPage = () => {
+  const [viewModal, updateModal] = useState<Boolean>(false);
+
   return (
     <div className="w-11/12 mx-auto">
-      <Modal name={"Header Text"} />
-      <p className="text-center">Hello</p>
+      <Modal
+        name={"Header Text"}
+        display={viewModal}
+        updateDisplay={updateModal}
+      />
+      <div className="text-center">
+        <button
+          onClick={() => {
+            updateModal(true);
+          }}
+        >
+          Hello
+        </button>
+      </div>
     </div>
   );
 };
