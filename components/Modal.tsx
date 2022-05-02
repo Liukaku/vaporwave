f42d5917ef91841ea019381e140648ac4c6afa58
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-05-02 11:38:10 +0100

    added animations

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Icon from "../modalIcon.svg";
 import Draggable from "react-draggable";
+import { motion } from "framer-motion";
 
 interface Props {
   name: String;
@@ -12,42 +13,51 @@ interface Props {
 const Modal = (props: Props) => {
   const toggleMouse = (e: React.MouseEvent, upDown: Boolean) => {
     const target = e.target as HTMLDivElement;
+    console.log(e.target);
     if (upDown) {
       target.classList.add("cursor-grabbing");
+      target.classList.remove("cursor-grab");
     } else {
       target.classList.remove("cursor-grabbing");
+      target.classList.add("cursor-grab");
     }
   };
 
   return (
     <Draggable handle=".headerBar">
-      <div
-        className={`absolute z-50 w-4/6 p-0.5  mx-auto  bg-pink-300 border-2 border-t-pink-200 border-r-pink-200 border-l-pink-400 border-b-pink-400 ${
-          props.display ? `` : `hidden`
-        }`}
-      >
-        <div
-          className="headerBar cursor-grab p-1 text-xl leading-none"
-          onMouseDown={(e) => {
-            toggleMouse(e, true);
-          }}
-          onMouseUp={(e) => {
-            toggleMouse(e, false);
-          }}
+      <div>
+        <motion.div
+          initial={{ y: 1080, x: 100, opacity: 0, width: 0 }}
+          animate={{ y: 0, x: 0, opacity: 100, width: "100%" }}
+          exit={{ y: 1080, x: 100, opacity: 0, width: 0 }}
+          transition={{ duration: 1, ease: "anticipate" }}
+          className={`absolute z-50 w-4/6 p-0.5  mx-auto  bg-pink-300 border-2 border-t-pink-200 border-r-pink-200 border-l-pink-400 border-b-pink-400 ${
+            props.display ? `` : `hidden`
+          }`}
         >
-          <Image className="mr-10" src={Icon} height={"25"} width={"25"} />
-          {props.name}
-
-          <button
-            className="h-6 w-6 bg-gray-300 float-right border-2 border-t-gray-100 border-r-gray-100 border-l-gray-600 border-b-gray-600 font-black text-base leading-none"
-            onClick={() => props.updateDisplay(false)}
+          <div
+            className="headerBar cursor-grab p-1 text-xl leading-none"
+            onMouseDown={(e) => {
+              toggleMouse(e, true);
+            }}
+            onMouseUp={(e) => {
+              toggleMouse(e, false);
+            }}
           >
-            X
-          </button>
-        </div>
-        <div className="border-t-2 border-pink-300 bg-slate-400">
-          <p>Fack off</p>
-        </div>
+            <Image className="mr-10" src={Icon} height={"25"} width={"25"} />
+            {props.name}
+
+            <button
+              className="h-6 w-6 bg-gray-300 float-right border-2 border-t-gray-100 border-r-gray-100 border-l-gray-600 border-b-gray-600 font-black text-base leading-none"
+              onClick={() => props.updateDisplay(false)}
+            >
+              X
+            </button>
+          </div>
+          <div className="border-t-2 border-pink-300 bg-slate-400">
+            <p>Fack off</p>
+          </div>
+        </motion.div>
       </div>
     </Draggable>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
 import Head from "next/head";
 import Modal from "../components/Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Draggable from "react-draggable";
 
 const IndexPage = () => {
   const [viewModal, updateModal] = useState<Boolean>(false);
+  useEffect(() => {
+    console.log("mounted");
+    return () => {
+      console.log("unmounted");
+    };
+  }, []);
 
   return (
     <div className="w-11/12 mx-auto">
-      <Modal
-        name={"Header Text"}
-        display={viewModal}
-        updateDisplay={updateModal}
-      />
       <div className="text-center">
         <button
           onClick={() => {
@@ -22,6 +25,17 @@ const IndexPage = () => {
           Hello
         </button>
       </div>
+      <AnimatePresence>
+        {viewModal ? (
+          <Modal
+            name={"Header Text"}
+            display={true}
+            updateDisplay={updateModal}
+          />
+        ) : (
+          ""
+        )}
+      </AnimatePresence>
     </div>
   );
 };
